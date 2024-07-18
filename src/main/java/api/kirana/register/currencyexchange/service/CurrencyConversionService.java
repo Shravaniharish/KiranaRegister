9600387c450928c615171e474b08b6281a644db8
Shravani H <shravanih@JAR-LT-194.local>
2024-07-18 17:04:39 +0530

    empty branch

--- src/main/java/api/kirana/register/currencyexchange/service/CurrencyConversionService.java
+++
@@ -1,9 +0,0 @@
-package api.kirana.register.currencyexchange.service;
-
-public interface CurrencyConversionService {
-    double getAmountById(String id);
-
-    String convertCurrency(String id,String baseCurrency, String targetCurrency);
-
-
-}
--- src/main/java/api/kirana/register/store/repo/StoreRepository.java
+++
@@ -1,9 +0,0 @@
-package api.kirana.register.store.repo;
-
-import api.kirana.register.store.models.Store;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface StoreRepository extends MongoRepository<Store , String> {
-
-}
