a5006279a1833e3d4d66bf3d3fc0465d3da6864c
Shravani H <shravanih@JAR-LT-194.local>
2024-08-01 16:27:28 +0530

    removed unnecessary code

--- src/main/java/api/kirana/register/RegisterApplication.java
+++ src/main/java/api/kirana/register/RegisterApplication.java
@@ -2,12 +2,10 @@ package api.kirana.register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 
 @SpringBootApplication
-@EnableCaching
 @EnableMongoAuditing
 public class RegisterApplication {
 
